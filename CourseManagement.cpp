a331b11b9477dbc65202699186976cd4813cb551
Mahmoud Kandil <mahmoud2018170364@cis.asu.edu.eg>
2019-12-13 19:00:55 +0200

    Revert "admin"
    This reverts commit f26305b169fc2e842034ca48cfe3c0fac9d52a90.

--- CourseManagement.cpp
+++ CourseManagement.cpp
@@ -14,7 +14,6 @@ CourseManagement::CourseManagement(QWidget *parent)
 }
 void CourseManagement::on_pushButton_clicked() {
 	string username = ui.lineEdit->text().toStdString();
-	transform(username.begin(), username.end(), username.begin(), ::tolower);
 	string password = ui.lineEdit_2->text().toStdString();
 	auto user = Database::GetUserByUsername(username);
 	if (user == nullptr)
