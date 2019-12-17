c751fc1254eca7fb1bd52631fb9b7735c07506a3
Mahmoud Mostafa <hoda002009@hotmail.com>
2019-12-17 18:33:15 +0200

    fixes

--- adminPanel.cpp
+++ adminPanel.cpp
@@ -1,5 +1,6 @@
 #include <QMessageBox>
 #include "adminPanel.h"
+#include"CSVFile.h"
 adminPanel::adminPanel(User* admin,QWidget *parent)
 	: QWidget(parent)
 {
@@ -31,7 +32,8 @@ adminPanel::~adminPanel()
 void adminPanel::on_addsbtn_clicked()
 {	
 	QMessageBox msgBox;
-	if (ui.newsname->text().isEmpty() || ui.newspw->text().isEmpty() || ui.newsrolecb->currentIndex()<0 || ui.newsun->text().isEmpty() || ui.newsyear->text().isEmpty())
+	if (ui.newsname->text().isEmpty() || ui.newspw->text().isEmpty() || ui.newsrolecb->currentIndex() < 0 ||
+		ui.newsun->text().isEmpty() || ui.newsyear->text().isEmpty())
 	{
 		msgBox.setWindowTitle("No Data Entered");
 		msgBox.setText("Please Enter User Data");
@@ -40,15 +42,17 @@ void adminPanel::on_addsbtn_clicked()
 	}
 	if (ui.newsrolecb->currentIndex() == 1)
 	{
-		User::User(ui.newsname->text().toStdString(), ui.newsun->text().toStdString(), ui.newspw->text().toStdString(), ui.newsrolecb->currentIndex());//admin
+		auto u = new User(ui.newsname->text().toStdString(), ui.newsun->text().toStdString(), ui.newspw->text().toStdString(),
+		           ui.newsrolecb->currentIndex()); //admin
 		msgBox.setWindowTitle("User Added");
 		msgBox.setText("Admin Added Successfuly");
 		msgBox.exec();
 	}
 	else if (ui.newsrolecb->currentIndex() == 0)
 	{
-		vector<string> empty; // 3aiz azbot vector l string da
-		Student::Student(ui.newsname->text().toStdString(), ui.newsun->text().toStdString(), ui.newspw->text().toStdString(), ui.newsyear->text().toInt(), empty, empty);//student
+		auto u = new Student(ui.newsname->text().toStdString(), ui.newsun->text().toStdString(),
+		                     ui.newspw->text().toStdString(), ui.newsyear->text().toInt(), vector<string>(),
+		                     vector<string>()); //student
 		msgBox.setWindowTitle("User Added");
 		msgBox.setText("Student Added Successfuly");
 		msgBox.exec();
@@ -57,15 +61,17 @@ void adminPanel::on_addsbtn_clicked()
 void adminPanel::on_addcbtn_clicked()
 {
 	QMessageBox msgBox;
-	if(ui.newcname->text().isEmpty()||ui.newchrs->text().isEmpty()||ui.newcid->text().isEmpty()||ui.newcstd->text().isEmpty())
+	if(ui.newcname->text().isEmpty()||ui.newchrs->text().isEmpty()||ui.newcid->text().isEmpty()||
+		ui.newcstd->text().isEmpty())
 	{
 		msgBox.setWindowTitle("No Data Entered");
 		msgBox.setText("Please Enter Course Data");
 		msgBox.exec();
 		return;
 	}
-	vector<string> empty; //ana hna ba5od id l course bs ahna 3aizeno zi id l user byzed 1 kol mara ++ 3aiz azbot required courses
-	Course::Course(ui.newcname->text().toStdString(), ui.newcid->text().toStdString(), ui.newcstd->text().toInt(), ui.newchrs->text().toInt(), empty);
+	auto PreReq = CSVFile::ParseLine(ui.newcprc->text().toStdString());
+	auto c = new Course(ui.newcname->text().toStdString(), ui.newcid->text().toStdString(), ui.newcstd->text().toInt(),
+	                    ui.newchrs->text().toInt(), PreReq);
 	msgBox.setWindowTitle("Course Added");
 	msgBox.setText("Course Added Successfuly");
 	msgBox.exec();
